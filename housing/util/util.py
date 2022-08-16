351690b9436b246b8ee9e6507a4b1e1a1bafb377
AKINTAYO AKINPELU <AKINTAYO.AKINPELU@TRANTERIT.LOCAL>
2022-08-16 10:27:48 +0100

    template

--- housing/util/util.py
+++ housing/util/util.py
@@ -1,3 +1,4 @@
+
 import yaml
 from housing.exception import HousingException
 import os,sys
@@ -5,6 +6,22 @@ import numpy as np
 import dill
 import pandas as pd
 from housing.constant import *
+
+
+def write_yaml_file(file_path:str,data:dict=None):
+    """
+    Create yaml file 
+    file_path: str
+    data: dict
+    """
+    try:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path,"w") as yaml_file:
+            if data is not None:
+                yaml.dump(data,yaml_file)
+    except Exception as e:
+        raise HousingException(e,sys)
+
 
 def read_yaml_file(file_path:str)->dict:
     """
@@ -93,3 +110,4 @@ def load_data(file_path: str, schema_file_path: str) -> pd.DataFrame:
 
     except Exception as e:
         raise HousingException(e,sys) from e
+    
